d330d0295a9dd7ff0febe86803b2ad9743a47e7b
gegebe <ggirbes@gmail.com>
2023-11-08 13:39:19 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
-import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.global.js'
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
 createApp({
   setup() {
 
+    const billAmount = ref("");
+    const percentage = ref("");
+    const tipAmount = ref("");
+    const total = ref("");
+
+    const calculateTip = () => {
+      tipAmount.value = billAmount.value * percentage.value / 100;
+      total.value = billAmount.value + tipAmount.value;
+    }
+
     return {
+      billAmount,
+      percentage,
+      tipAmount,
+      total,
+      calculateTip,
 
     }
   }
